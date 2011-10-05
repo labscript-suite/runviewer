ca7bb031a1aa478f4b632059f85bcf81425985d8
Chris Billington <chrisjbillington@gmail.com>
2011-10-05 07:49:12 +0000

    change to setup.py to hopefully make it compile on windows

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from distutils.core import setup, Extension
+import numpy
 
 setup(
     ext_modules = [
-        Extension("resample",sources=["resample.c"])
+        Extension("resample",sources=["resample.c"], include_dirs = [numpy.get_include()])
         ]
     )
     
