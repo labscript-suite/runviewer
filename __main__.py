4373b711aca32ad6b8fad612d779f3f372ffebd3
Chris Billington <chrisjbillington@gmail.com>
2013-01-04 01:21:40 +0000

    Committing tab experimentation

--- __main__.py
+++ __main__.py
@@ -15,29 +15,51 @@ pg.setConfigOption('useOpenGL', False)
 pen = pg.mkPen('k', width=2)
 
 class DragDropTabBar(QTabBar):
-    def __init__(self, *args, **kwargs):
-        QTabBar.__init__(self, *args, **kwargs)
+    def __init__(self, tab_widget):
+        QTabBar.__init__(self)
         self.setAcceptDrops(True)
-        
+        self.tab_widget = tab_widget
+        self.tab_widget.setTabBar(self)
+        self.tab_widget.setMovable(True)
+                
     def mousePressEvent(self, e):
         print 'mousepress', e
         QTabBar.mousePressEvent(self, e)
         
     def mouseMoveEvent(self, e):
-        print 'mousemove', e
-        drag = QDrag(self)
         
-        mimeData = QMimeData()
-
-        drag.setMimeData(mimeData)
-        dropAction = drag.start(Qt.MoveAction)
+        if (self.x() < e.x() < (self.x() + self.width()) and 
+            self.y() < e.y() < (self.y() + self.height())):
+            print 'in!'
+            QTabBar.mouseMoveEvent(self, e)
+        else:
+            print 'out!'
+            current_index = self.currentIndex()
+            print current_index
+            text = self.tabText(current_index)
+            icon = self.tabIcon(current_index)
+            widget = self.tab_widget.currentWidget()
+            self.moving_tab = text, icon, widget
+            print text, icon, widget
+            self.removeTab(current_index)
+            drag = QDrag(self)
+            mimeData = QMimeData()
+            drag.setMimeData(mimeData)
+            dropAction = drag.start(Qt.MoveAction)
+            print 'foo!'
+    
+    def dragMoveEvent(self, e):
         QTabBar.mouseMoveEvent(self, e)
-        
-        
+        print 'drag move'
+    
+    def dragLeaveEvent(self, e):
+        print 'drag leave'
+            
     def dragEnterEvent(self, e):
+        text, icon, widget = self.moving_tab
+        self.tab_widget.addTab(widget, icon, text)
+        print text, icon, widget
         print 'dragenter', e
-
-#        QTabBar.dragEnterEvent(self,e)
         e.accept()
         
     def dropEvent(self, e):
@@ -48,9 +70,7 @@ class ViewPort(object):
     def __init__(self, container_layout):
         #ui = QUiLoader().load('viewport.ui')
         tab_widget = QTabWidget()
-        tab_bar = DragDropTabBar()
-        tab_widget.setTabBar(tab_bar)
-        tab_widget.setMovable(True)
+        tab_bar = DragDropTabBar(tab_widget)
                 
         container_layout.addWidget(tab_widget)
         tab_widget.addTab(QWidget(), 'foo')
@@ -75,14 +95,14 @@ class RunViewer(object):
         
         self.window.show()
         
-        shot = runviewer.Shot('/home/bilbo/20121023T000422_crossed_beam_bec_09.h5')
-        trace = shot.traces[0]
-        x, y = trace.times, trace.data
-        x, y = runviewer.resample(x,y,x.min(),x.max(),100000)
-        
-        win = pg.GraphicsWindow(title="Basic plotting examples")
-        win.resize(800,600)
-        p1 = win.addPlot(title="Basic array plotting", x=x, y=y, pen=pen)
+#        shot = runviewer.Shot('/home/bilbo/20121023T000422_crossed_beam_bec_09.h5')
+#        trace = shot.traces[0]
+#        x, y = trace.times, trace.data
+#        x, y = runviewer.resample(x,y,x.min(),x.max(),100000)
+#        
+#        win = pg.GraphicsWindow(title="Basic plotting examples")
+#        win.resize(800,600)
+#        p1 = win.addPlot(title="Basic array plotting", x=x, y=y, pen=pen)
 
 if __name__ == '__main__':
     qapplication = QApplication([])
