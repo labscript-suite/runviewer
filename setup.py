75d67748a4a9fac19beb004b1398f123420721e3
Chris Billington <chrisjbillington@gmail.com>
2013-07-22 17:45:56 +0200

    stable: Modified setup.py and readme.py to function correctly when the compiler is not default. This works with Python(x,y).

--- setup.py
+++ setup.py
@@ -7,10 +7,5 @@ setup(
         ]
     )
     
-import shutil, os
-if os.name == 'posix':
-    shutil.copy('build/lib.linux-x86_64-2.6/resample.so','resample.so')
-elif os.name == 'nt':
-    shutil.copy('build/lib.win32-2.7/resample.pyd','resample.pyd')
-
+import shutil
 shutil.rmtree('build')
