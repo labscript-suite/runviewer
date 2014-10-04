9eb5082c56cf57559d9db1aca7e9dc11fa083acd
Chris Billington <chrisjbillington@gmail.com>
2014-10-04 17:04:51 -0400

    Added __version__ attribute to __init__.py

---
+++ __init__.py
@@ -0,0 +1,14 @@
+#####################################################################
+#                                                                   #
+# /__init__.py                                                      #
+#                                                                   #
+# Copyright 2013, Monash University                                 #
+#                                                                   #
+# This file is part of the program runviewer, in the labscript     #
+# suite (see http://labscriptsuite.org), and is licensed under the  #
+# Simplified BSD License. See the license.txt file in the root of   #
+# the project for the full license.                                 #
+#                                                                   #
+#####################################################################
+
+__version__ = '2.0.0'
