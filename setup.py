dfab0799b8114b7365340a1bca3c465ad43623d6
Chris Billington <chrisjbillington@gmail.com>
2019-11-29 23:25:53 -0500

    restructure: Dependencies in setup.py, and other necessary changes to get an upload to PyPI working.

--- setup.py
+++ setup.py
@@ -1,18 +1,48 @@
+# Make a release tarball with:
+#
+#     `python setup.py sdist`
+#
+# Upload to test PyPI with:
+#
+#     `twine upload --repository-url https://test.pypi.org/legacy/ dist/*`
+#
+# Install from test PyPI with:
+#
+#     pip install --index-url https://test.pypi.org/simple/ runviewer
+#
+# Upload to real PyPI with:
+#
+#     `twine upload dist/*`
+#
+
 from setuptools import setup
 
 from runviewer import __version__
 
-DESCRIPTION = ("A program to view shots compiled by labscript" )
+#TODO: add labscript suite deps once they are on PyPI
+REQUIREMENTS = [
+    "autocython",
+    "zprocess",
+    "qtutils >= 2.0.0",
+    "pyqtgraph >= 0.9.10",
+    "numpy",
+    "scipy",
+    "h5py"
+]
 
-setup(name='runviewer',
-      version=__version__,
-      description=DESCRIPTION,
-      long_description=open('README.md').read(),
-      long_description_content_type='text/markdown',
-      author='The labscript suite community',
-      author_email='labscriptsuite@googlegroups.com ',
-      url='labscriptsuite.org',
-      license="BSD",
-      py_modules=["inotify_simple"],
-      python_requires=">2.7, !=3.0.*, !=3.1.*"
-      )
+setup(
+    name='runviewer',
+    version=__version__,
+    description="A program to view shots compiled by labscript",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='The labscript suite community',
+    author_email='labscriptsuite@googlegroups.com ',
+    url='http://labscriptsuite.org',
+    license="BSD",
+    packages=["runviewer", "runviewer.resample"],
+    package_data={'': ['*']},
+    include_package_data=True,
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    install_requires=REQUIREMENTS
+)
