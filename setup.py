5226a7a292105cb0cf211352bb99ca740f363f16
Russell Anderson <5637107+rpanderson@users.noreply.github.com>
2020-05-14 22:39:44 +1000

    Automated build and release per rpanderson/workflow-sandbox 0.3.0

--- setup.py
+++ setup.py
@@ -22,6 +22,12 @@ VERSION_SCHEME["version_scheme"] = os.environ.get(
 )
 VERSION_SCHEME["local_scheme"] = os.environ.get("SCM_LOCAL_SCHEME", "node-and-date")
 
+VERSION_SCHEME = {}
+VERSION_SCHEME["version_scheme"] = os.environ.get(
+    "SCM_VERSION_SCHEME", "release-branch-semver"
+)
+VERSION_SCHEME["local_scheme"] = os.environ.get("SCM_LOCAL_SCHEME", "node-and-date")
+
 setup(
     use_scm_version=VERSION_SCHEME,
     cmdclass=CMDCLASS,
